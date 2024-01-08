8a49db7db42ce9befdcf27dfc6dbcbc20b56372a
Parker <pkroberg@gmail.com>
2024-01-07 23:58:05 -0700

    Comments

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+//large tweet component
 
 const Tweet = (props) => (
   <div>
     <h1>{props.name}</h1>
-    <h1>{props.tweet}</h1>
+    <h2>{props.tweet}</h2>
   </div>
 );
 
